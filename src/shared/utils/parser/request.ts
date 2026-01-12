6ec0caf3c2a6d464fdab7001f56c1fdbe906012a
ggalmury <body0922@naver.com>
2026-01-13 00:30:06 +0900

    fix: request/parseJsonOrThrow의 파라미터 타입 변경

--- src/shared/utils/parser/request.ts
+++ src/shared/utils/parser/request.ts
@@ -1,8 +1,6 @@
-import { NextRequest } from 'next/server';
-
 import ResponseParseFailedError from '@/shared/errors/client/response-parse-failed-error';
 
-export const parseJsonOrThrow = async <T>(request: NextRequest): Promise<T> => {
+export const parseJsonOrThrow = async <T>(request: Request): Promise<T> => {
   try {
     return (await request.json()) as T;
   } catch {
