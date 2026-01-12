e1c4137ebeeca96ed876897107516347c2392067
ggalmury <body0922@naver.com>
2026-01-12 23:29:55 +0900

    feat: 공용 api client 구현

--- src/shared/utils/parser/request.ts
+++ src/shared/utils/parser/request.ts
@@ -2,9 +2,9 @@ import { NextRequest } from 'next/server';
 
 import ResponseParseFailedError from '@/shared/errors/client/response-parse-failed-error';
 
-export const parseJsonOrThrow = async (request: NextRequest): Promise<unknown> => {
+export const parseJsonOrThrow = async <T>(request: NextRequest): Promise<T> => {
   try {
-    return await request.json();
+    return (await request.json()) as T;
   } catch {
     throw new ResponseParseFailedError();
   }
--- src/shared/utils/url.ts
+++ src/shared/utils/url.ts
@@ -1,7 +1,3 @@
-export const normalizeOrigin = (origin: string): string => {
-  return origin.replace(/\/+$/, '');
-};
-
 export const normalizeEndpoint = (endpoint: string): string => {
   return endpoint.replace(/^\/+/, '');
 };
