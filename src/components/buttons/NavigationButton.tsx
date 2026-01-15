5081cad8e162ccfb4c859cf3812f17c1bb997cf4
ggalmury <body0922@naver.com>
2026-01-15 16:47:03 +0900

    feat: header 구현

---
+++ src/components/buttons/NavigationButton.tsx
@@ -0,0 +1,32 @@
+'use client';
+
+import { ButtonHTMLAttributes } from 'react';
+import { usePathname, useRouter } from 'next/navigation';
+
+import { cn } from '@/shared/utils/cn';
+
+interface NavigationButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  label: string;
+  to: string;
+}
+
+const NavigationButton = ({ label, to, ...rest }: NavigationButtonProps) => {
+  const router = useRouter();
+  const pathname = usePathname();
+
+  return (
+    <button
+      {...rest}
+      className={cn(
+        'rounded-lg px-6 py-2 text-[1.25rem] font-medium text-gray-300',
+        'hover:bg-gray-100',
+        pathname === to && 'font-bold text-gray-900',
+      )}
+      onClick={() => router.push(to)}
+    >
+      {label}
+    </button>
+  );
+};
+
+export default NavigationButton;
