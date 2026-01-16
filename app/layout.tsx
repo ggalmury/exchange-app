dcfc4531f4a3cf52886819e382eba6f9a274fd29
ggalmury <body0922@naver.com>
2026-01-16 18:09:30 +0900

    feat: pretendard 폰트 적용

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
+import localFont from 'next/font/local';
 
 import '@/styles/global.css';
 
@@ -8,14 +8,10 @@ import { cn } from '@/shared/utils/cn';
 import ToastProvider from '@/providers/ToastProvider';
 import QueryProvider from '@/providers/QueryProvider';
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
+export const pretendard = localFont({
+  src: '../public/fonts/PretendardVariable.woff2',
+  variable: '--font-pretendard',
+  display: 'swap',
 });
 
 export const metadata: Metadata = {
@@ -30,7 +26,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={cn('min-h-screen antialiased', geistSans.variable, geistMono.variable)}>
+      <body className={cn('min-h-screen antialiased', pretendard.className)}>
         <ToastProvider />
         <QueryProvider>{children}</QueryProvider>
       </body>
